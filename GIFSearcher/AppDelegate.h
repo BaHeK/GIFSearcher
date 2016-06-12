8034d923cc262bc13c309b3e02a7e4354511f00c
Ivan <master.tv@gmail.com>
2016-06-12 12:29:31 +0300

    Initial Commit

---
+++ GIFSearcher/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIFSearcher
+//
+//  Created by Ivan on 12.06.16.
+//  Copyright © 2016 i_sokolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIFSearcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GIFSearcher
+//
+//  Created by Ivan on 12.06.16.
+//  Copyright © 2016 i_sokolov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
